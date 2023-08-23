8700bc635a4ebfe61f6b23b513540d3954ce9396
Ricardo <ricardoclpaiva@gmail.com>
2023-08-22 22:27:43 -0300

    Implemented some unit tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,45 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void test_agedBrieIncreasesInQualityTheOlderItGets() {
+        // Given
+        int numDays = 10;
+
+        // Items
+        Item agedBrie = new Item("Aged Brie", 120, 20);
+
+        // App
+        Item[] items = new Item[] {agedBrie};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        for (int day = 0; day < numDays; day++) {
+            app.updateQuality();
+        }
+
+        // Then
+        assertEquals(30, agedBrie.quality);
+    }
+
+    @Test
+    void test_sulfurasNeverDecreaseQuality() {
+        // Given
+        int numDays = 10;
+        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        Item[] items = new Item[] { sulfuras };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        for (int day = 0; day < numDays; day++) {
+            app.updateQuality();
+        }
+
+        // Then
+        assertEquals(80, sulfuras.quality);
+
+    }
+
+
+
 }
